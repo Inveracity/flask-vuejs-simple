af6a699f576fca50766dd3444db9808593cdb58e
Christopher Baklid <christopherb@unity3d.com>
2018-03-10 10:35:28 +0100

    update package, support python3

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ app = Flask(__name__, template_folder='dist', static_folder='dist/static')
 
 @app.route('/')
 def index():
-    print request.remote_addr
+    print(request.remote_addr)
 
     return render_template('index.html')
 
@@ -53,38 +53,3 @@ def page_not_found(*args, **kwargs):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081, debug=True)
-
-
-'''
-# Written by Christopher Baklid
-# This code can open a port on the Sagem F@st 3666 home router without having to log in.
-# The first request creates a new table entry
-# The second request inputs the ports and ip addresses and the stuff we need
-
-import requests
-url = 'http://192.168.0.1/goform/RgForwarding'
-
-precursor = {
-    'PortForwardingCreateRemove'  : '1',
-    'PortForwardingTable'         : '0',
-}
-
-payload = {
-    'PortForwardingCreateRemove'  : '0',
-    'PortForwardingLocalIp'       : '192.168.0.3',
-    'PortForwardingLocalStartPort': '8081',
-    'PortForwardingLocalEndPort'  : '8081',
-    'PortForwardingExtStartPort'  : '8081',
-    'PortForwardingExtEndPort'    : '8081',
-    'PortForwardingProtocol'      : '254',
-    'PortForwardingDesc'          : 'sessorium',
-    'PortForwardingEnabled'       : '1',
-    'PortForwardingApply'         : '2',
-    'PortForwardingTable'         : '0',
-}
-
-headers = {'Content-Type':'application/x-www-form-urlencoded'}
-r = requests.post(url, data=precursor, headers=headers)
-r = requests.post(url, data=payload, headers=headers)
-print r.text
-'''
